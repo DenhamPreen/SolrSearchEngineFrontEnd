b880a2ea331f8ea98a51e50b2dd137b69d6def85
Sabelo Xulu <cacofonixt@gmail.com>
2017-11-05 18:45:30 +0200

    ngrok

--- js/solrServerCall.js
+++ js/solrServerCall.js
@@ -7,12 +7,12 @@ function getResponses(query) {
 	//example query: directed_by:'Gary Lennon', q=id:"USD"
   var xhttp = new XMLHttpRequest();
   console.log(query);
-  var url = 'http://localhost:'+ config.port +'/solr/'+ config.core +'/select?q='+query;
+  var url = 'http://93ee75a7.ngrok.io'+'/solr/'+ config.core +'/browse?q='+query;
 	//var url = 'http://localhost:8983/solr/testCore/select?q=id:"USD"';
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     	//document.getElementById("results").innerHTML = this.response;
-        window.location = 'http://localhost:'+ config.port +'/solr/'+ config.core +'/browse?q='+query;
+     	document.getElementById("results").innerHTML = this.response;
+        //window.location = 'http://localhost:'+ config.port +'/solr/'+ config.core +'/browse?q='+query;
     }
   };
   xhttp.open("GET", url, true);
