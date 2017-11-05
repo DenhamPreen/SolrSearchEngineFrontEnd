3cee5384d57d71a6d7e56246f4b0fac307b204ee
Sabelo Xulu <cacofonixt@gmail.com>
2017-11-05 17:31:04 +0200

    Minor changes to code
    Added redirect for results generation

--- js/solrServerCall.js
+++ js/solrServerCall.js
@@ -1,17 +1,18 @@
 var config = {
-	core: 'testCore',	
+	core: 'testRecords',	
 	port: '8983'
 }
 
 function getResponses(query) {
 	//example query: directed_by:'Gary Lennon', q=id:"USD"
   var xhttp = new XMLHttpRequest();
-	console.log(query);
+  console.log(query);
   var url = 'http://localhost:'+ config.port +'/solr/'+ config.core +'/select?q='+query;
 	//var url = 'http://localhost:8983/solr/testCore/select?q=id:"USD"';
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     	document.getElementById("results").innerHTML = this.response;
+     	//document.getElementById("results").innerHTML = this.response;
+        window.location = 'http://localhost:'+ config.port +'/solr/'+ config.core +'/browse?q='+query;
     }
   };
   xhttp.open("GET", url, true);
