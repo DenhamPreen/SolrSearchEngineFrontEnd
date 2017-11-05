ae2867519db94aa70eb272dd957e1292adc4e127
Denham Preen <denhampreen@gmail.com>
2017-11-05 12:23:00 +0200

    working for given url

--- js/solrServerCall.js
+++ js/solrServerCall.js
@@ -6,7 +6,8 @@ var config = {
 function getResponses(query) {
 	//example query: directed_by:'Gary Lennon'
   var xhttp = new XMLHttpRequest();
-  var url = "http://localhost:"+ config.port +"/solr/"+ config.core +"/select?indent=on&q="+query+"&wt=json";
+  //var url = "http://localhost:"+ config.port +"/solr/"+ config.core +"/select?indent=on&q="+query+"&wt=json";
+	var url = 'http://localhost:8983/solr/testCore/select?q=id:"USD"';
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      	document.getElementById("results").innerHTML = this.response;
